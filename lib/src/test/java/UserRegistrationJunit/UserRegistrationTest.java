cdd1a136ecf310a1415f45dbe7ef086fcadf14d8
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-13 12:22:50 +0530

    Added : Test cases to check first name

--- lib/src/test/java/UserRegistrationJunit/UserRegistrationTest.java
+++ lib/src/test/java/UserRegistrationJunit/UserRegistrationTest.java
@@ -1,6 +1,32 @@
 package UserRegistrationJunit;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class UserRegistrationTest 
 {
+	//checking conditions for first name
+		@Test
+		public void givenFirstName_WhenProper_ShouldReturnValid() 
+		{
+			UserRegistrationService firstNamevalidator = new UserRegistrationService();
+			assertTrue(firstNamevalidator.checkFirstName("Abcd"));			
+		}
+
+		@Test
+		public void givenFirstName_WhenShort_ShouldReturnValid() 
+		{
+			UserRegistrationService firstNamevalidator = new UserRegistrationService();
+			assertFalse(firstNamevalidator.checkFirstName("Ab"));			
+		}
+
+		@Test
+		public void givenFirstName_WhenFirstNotInUpperCase_ShouldReturnValid() 
+		{
+			UserRegistrationService firstNamevalidator = new UserRegistrationService();
+			assertFalse(firstNamevalidator.checkFirstName("abcd"));			
+		}
 	
 }
