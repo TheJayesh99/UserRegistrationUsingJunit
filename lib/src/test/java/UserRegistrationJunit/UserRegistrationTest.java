c28221604135dfd745156cfc9e7f11abfdee89fd
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-13 12:35:38 +0530

    Added : Test cases to check last name

--- lib/src/test/java/UserRegistrationJunit/UserRegistrationTest.java
+++ lib/src/test/java/UserRegistrationJunit/UserRegistrationTest.java
@@ -8,25 +8,47 @@ import org.junit.Test;
 public class UserRegistrationTest 
 {
 	//checking conditions for first name
-		@Test
-		public void givenFirstName_WhenProper_ShouldReturnValid() 
-		{
-			UserRegistrationService firstNamevalidator = new UserRegistrationService();
-			assertTrue(firstNamevalidator.checkFirstName("Abcd"));			
-		}
+	@Test
+	public void givenFirstName_WhenProper_ShouldReturnValid() 
+	{
+		UserRegistrationService firstNamevalidator = new UserRegistrationService();
+		assertTrue(firstNamevalidator.checkFirstName("Abcd"));			
+	}
 
-		@Test
-		public void givenFirstName_WhenShort_ShouldReturnValid() 
-		{
-			UserRegistrationService firstNamevalidator = new UserRegistrationService();
-			assertFalse(firstNamevalidator.checkFirstName("Ab"));			
-		}
+	@Test
+	public void givenFirstName_WhenShort_ShouldReturnValid() 
+	{
+		UserRegistrationService firstNamevalidator = new UserRegistrationService();
+		assertFalse(firstNamevalidator.checkFirstName("Ab"));			
+	}
 
-		@Test
-		public void givenFirstName_WhenFirstNotInUpperCase_ShouldReturnValid() 
-		{
-			UserRegistrationService firstNamevalidator = new UserRegistrationService();
-			assertFalse(firstNamevalidator.checkFirstName("abcd"));			
-		}
+	@Test
+	public void givenFirstName_WhenFirstNotInUpperCase_ShouldReturnValid() 
+	{
+		UserRegistrationService firstNamevalidator = new UserRegistrationService();
+		assertFalse(firstNamevalidator.checkFirstName("abcd"));			
+	}
+
+	//checking conditions for last name
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnValid() 
+	{
+		UserRegistrationService lastNamevalidator = new UserRegistrationService();
+		assertTrue(lastNamevalidator.checkLastName("Abcd"));			
+	}
 	
+	@Test
+	public void givenLastName_WhenShort_ShouldReturnValid() 
+	{
+		UserRegistrationService lastNamevalidator = new UserRegistrationService();
+		assertFalse(lastNamevalidator.checkLastName("Ab"));			
+	}
+	
+	@Test
+	public void givenLastName_WhenFirstNotInUpperCase_ShouldReturnValid() 
+	{
+		UserRegistrationService lastNamevalidator = new UserRegistrationService();
+		assertFalse(lastNamevalidator.checkLastName("abcd"));			
+	}
+
 }
